6ed4d093c95f6e49485eec87881224e3d2d2eb93
Md. Samin Irtiza <131145579+samin-irtiza-bjit@users.noreply.github.com>
2023-06-11 18:50:50 +0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,12 +1,12 @@
 USE tetris;
-CREATE TABLE UserTable (
+CREATE TABLE Users (
   Username varchar(50) UNIQUE,
   FirstName varchar(50),
   LastName varchar(50),
   Password varchar(50),
   Display varchar(50)
 );
-CREATE TABLE ScoresTable (
+CREATE TABLE Scores (
   Username varchar(50),
   Score int
 );
