8a0c0c2d29f8df3dee9a37094cde4fdc9325f20c
Muhammad Mutasim Billah <130433248+mbahnaf@users.noreply.github.com>
2023-06-15 19:13:15 +0600

    Update connect.php

--- res/connect.php
+++ res/connect.php
@@ -1,9 +1,9 @@
 <?php
 $username = "root";
-$password = "bjit1234";
+$password = "qwerty123";
 $dbname = "tetris";
 // Create connection
-$conn = mysqli_connect("samin-db.c5tsce7xtv8b.eu-west-3.rds.amazonaws.com", $username, $password, $dbname);
+$conn = mysqli_connect("mbahnaf-db-2.cjfaahfgehcs.eu-central-1.rds.amazonaws.com", $username, $password, $dbname);
 // Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
